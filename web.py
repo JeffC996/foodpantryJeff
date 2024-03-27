f926278bf1160e157e54e22b5be86cc2022bbcd5
JeffC996 <144235880+JeffC996@users.noreply.github.com>
2024-03-27 15:24:07 -0400

    state the default port to be 0.0.0.0 for web.py

--- web.py
+++ web.py
@@ -7,5 +7,7 @@ app = Flask(__name__, static_folder='.', static_url_path='')
 def fill(type, category, brand):
     return app.send_static_file('index.html')
 
-if __name__ == '__main__':
-    app.run(port=8000)
+#if __name__ == '__main__':
+    #app.run(port=8000) the default port for app.run in Flask is 127.0.0.1, which doesn't allow external access.
+if __name__ == '__main__': 
+    app.run(host='0.0.0.0', port=8000)
